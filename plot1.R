109a88e8e94b3bd29879823b171ec62382da1dfb
Kamal Mishra <mishra.kamal@gmail.com>
2014-08-11 02:34:16 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,33 +7,33 @@
 ## Start of program code -- plot1.R
 ##
 
-## Step1: Get the full dataset
+	## Step1: Get the full dataset
 
-data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+	data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+	data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-## Step2: Subset the data
+	## Step2: Subset the data
 
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+	data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-rm(data_full)
+	rm(data_full)
 
-## Step3: Converting dates
+	## Step3: Converting dates
 
-datetime <- paste(as.Date(data$Date), data$Time)
+	datetime <- paste(as.Date(data$Date), data$Time)
 
-data$Datetime <- as.POSIXct(datetime)
+	data$Datetime <- as.POSIXct(datetime)
 
-## Step4: Plot the requisite graph - Plot1
+	## Step4: Plot the requisite graph - Plot1
 
-hist(data$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+	hist(data$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## Step5: Saving to a file
+	## Step5: Saving to a file
 
-dev.copy(png, file="plot1.png", height=480, width=480)
+	dev.copy(png, file="plot1.png", height=480, width=480)
 
-dev.off()
+	dev.off()
 
 ##
 ## End of program code -- plot1.R

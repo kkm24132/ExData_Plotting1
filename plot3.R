7a9c473a8b6a37f121b6ac798423925a0347b866
kkm24132 <mishra.kamal@gmail.com>
2014-08-11 03:50:40 +0530

    Updates for Plot3 files
    Updates for Plot3 files

--- plot3.R
+++ plot3.R
@@ -25,12 +25,12 @@ data$Datetime <- as.POSIXct(datetime)
 ## Step4: Plot the requisite graph - Plot3
 
 with(data, {
-  plot(Sub_metering_1~Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l",ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
 })
 
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),xjust=0)
 
 ## Step5: Saving to a file
 
